b3f42855ffe9677a68553ffb6dc07d52043a00b4
jesgogu27 <jesgogu@hotmail.com>
2020-09-17 00:26:07 -0500

    monty

--- monty.h
+++ monty.h
@@ -26,9 +26,9 @@ extern int val;
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -41,10 +41,9 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-
 
 int op_func(char *opcode, stack_t **stack, unsigned int line_num);
 void pile_up(char *reg);
